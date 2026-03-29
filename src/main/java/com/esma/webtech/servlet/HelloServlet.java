d4fddae3da973c01d9ca248b5c6d1ee83e76597f
Esma Marangoz <esmasevvalmarangoz@gmail.com>
2026-03-29 18:46:30 +0300

    Add first Hello servlet API endpoint

---
+++ src/main/java/com/esma/webtech/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.esma.webtech.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.print("{\"message\":\"Hello from Student Portfolio Manager API\"}");
+        out.flush();
+    }
+}
