664c1fe284053f87d623114b177bfcf74406fb45
Esma Marangoz <esmasevvalmarangoz@gmail.com>
2026-04-02 23:50:30 +0300

    Load SQLite JDBC driver explicitly

--- src/main/java/com/esma/webtech/util/DBConnection.java
+++ src/main/java/com/esma/webtech/util/DBConnection.java
@@ -8,6 +8,14 @@ public class DBConnection {
 
     private static final String URL = "jdbc:sqlite:student_directory.db";
 
+    static {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("SQLite JDBC driver not found.", e);
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
